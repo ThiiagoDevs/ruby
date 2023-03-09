dfdda3f3c5202fd7db28bc1169f7b389757195d7
Thiago Ribeiro <thiagohsantana@icloud.com>
2023-03-09 01:22:33 -0300

    ruby

---
+++ repetições.rb
@@ -0,0 +1,43 @@
+#nomes = ["Jesus", "Pedro", "Paulo", "João", "Thiago"]
+
+#laço de repetição loop
+count = 1
+
+loop do
+    puts "Thiago"
+    puts count
+    if count == 10
+        break
+    end
+    count += 1
+end
+
+# laço de repetição times
+#count = 1
+#10.times do
+#    puts "Jesus"
+#    puts count
+#   count += 1
+#end
+
+#laço de repetição while
+#count = 0
+#while nomes.size do
+#    puts nomes[count]
+#    puts count
+#    count += 1
+#end
+
+
+#dict = {nome: "Moise", idade: 83, altura: 1.76, peso: 81.3}
+
+#laço de repetição for
+# for da lista 
+#for nome in nomes do
+    #puts nome
+#end
+
+#for k, v in dict do
+#    puts "#{k}: #{v}"
+#end
+
