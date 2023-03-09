17d3bebafba27fbffebc38648dede42149f020b6
Thiago Ribeiro <thiagohsantana@icloud.com>
2023-03-09 02:12:55 -0300

    ruby

---
+++ metodos.rb
@@ -0,0 +1,21 @@
+#metodo
+def soma(x, y)
+    return x + y
+end
+
+def subt(x, y)
+    return x - y
+end
+
+def mult(x, y)
+    return x * y
+end
+
+def div(x, y)
+    return x / y
+end
+
+puts soma 15, 25
+puts subt 100, 150
+puts mult 5, 8
+puts div 50, 5
