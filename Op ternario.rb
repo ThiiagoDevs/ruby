accd56920aa090a59aa0c044c3a7cc55f63a453b
Thiago Ribeiro <thiagohsantana@icloud.com>
2023-03-09 02:33:48 -0300

    ruby

---
+++ Op ternario.rb
@@ -0,0 +1,8 @@
+#nome = "Thiago"
+n = 4
+
+#resultado = nome.eql?("Thiago") ? "Dono do canal Thiago Code - resultado" : "Visitante - resultado"
+#puts resultado
+
+soma = n.eql?(5) ? n + 10 : n - 1
+puts soma
